cc31088c0d80d67be19823eca7ea123d68f53121
Max Obrist <obrist.max@gmail.com>
2022-09-25 00:38:04 +0800

    WIP value object and source generator / code fix analyzer

---
+++ SeedWork/tst/Domain.Tests/ValueObjectComparable.cs
@@ -0,0 +1,8 @@
+﻿////namespace Cranks.SeedWork.Domain.Tests;
+
+////public class ValueObjectComparableTest : UnitTestBase
+////{
+////    private record IntValueObject(int Value) : ValueObject<int>(Value);
+
+////    private record StringValueObject(string Value) : ValueObject<string>(Value);
+////}
---
+++ SeedWork/tst/Domain.Tests/ValueObjectTest.cs
@@ -0,0 +1,58 @@
+﻿using Shouldly;
+
+namespace Cranks.SeedWork.Domain.Tests;
+
+public class ValueObjectTest : UnitTestBase
+{
+    [Fact]
+    public void GivenValueObject_WhenInstantiated_IsInstantiated()
+    {
+        // arrange
+        var street = Faker.Address.StreetAddress();
+        var zipCode = Faker.Address.ZipCode();
+        var city = Faker.Address.City();
+
+        // act
+        var result = new Address(street, zipCode, city);
+
+        // assert
+        result.ShouldNotBeNull();
+        result.Street.ShouldBe(street);
+        result.ZipCode.ShouldBe(zipCode);
+        result.City.ShouldBe(city);
+    }
+
+    [Fact]
+    public void GivenTwoEqualValueObject_WhenCompared_AreEqual()
+    {
+        // arrange
+        var street = Faker.Address.StreetAddress();
+        var zipCode = Faker.Address.ZipCode();
+        var city = Faker.Address.City();
+
+        // act
+        var result1 = new Address(street, zipCode, city);
+        var result2 = new Address(street, zipCode, city);
+
+        // assert
+        result1.ShouldBe(result2);
+    }
+
+    [Fact]
+    public void GivenTwoDifferentValueObject_WhenCompared_AreNotEqual()
+    {
+        // arrange
+
+        // act
+        var result1 = new Address(Faker.Address.StreetAddress(), Faker.Address.ZipCode(), Faker.Address.City());
+        var result2 = new Address(Faker.Address.StreetAddress(), Faker.Address.ZipCode(), Faker.Address.City());
+
+        // assert
+        result1.ShouldNotBe(result2);
+    }
+
+    private record Address(string Street, string ZipCode, string City)
+        : ValueObject<Address>
+    {
+    }
+}
