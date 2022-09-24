f8d3c7cc13e1ac4693830bcbf96fc2ab21f2f8af
Max Obrist <obrist.max@gmail.com>
2022-09-25 00:38:04 +0800

    WIP value object and source generator / code fix analyzer

--- SeedWork/src/Domain/ValueObject.cs
+++ SeedWork/src/Domain/ValueObject.cs
@@ -1,4 +1,21 @@
 ﻿namespace Cranks.SeedWork.Domain;
 
-public abstract record ValueObject<T>(T Value)
-    where T : IEquatable<T>;
+public abstract record ValueObject<TValueObject>
+    where TValueObject : ValueObject<TValueObject>
+{
+}
+
+public abstract record ValueObjectUnary<T, TValueObject>(T Value)
+    where T : IEquatable<T>
+    where TValueObject : ValueObjectUnary<T, TValueObject>
+{
+    public static implicit operator T(ValueObjectUnary<T, TValueObject> source)
+    {
+        return source.Value;
+    }
+
+    public static implicit operator ValueObjectUnary<T, TValueObject>?(T? source)
+    {
+        return Activator.CreateInstance(typeof(TValueObject), new[] { source }) as TValueObject;
+    }
+}
--- SeedWork/src/Domain/ValueObjectComparable.cs
+++ SeedWork/src/Domain/ValueObjectComparable.cs
@@ -1,5 +1,53 @@
-﻿namespace Cranks.SeedWork.Domain;
+﻿////namespace Cranks.SeedWork.Domain;
 
-public abstract record ValueObjectComparable<T>(T Value)
-    : ValueObject<T>(Value)
-    where T : IEquatable<T>, IComparable<T>;
+////public abstract record ValueObjectComparable<T>(T Value)
+////    : ValueObject<T>(Value),
+////        IComparable<ValueObjectComparable<T>>,
+////        IComparable
+////    where T : IEquatable<T>, IComparable<T>
+////{
+////    public int CompareTo(object? obj)
+////    {
+////        if (ReferenceEquals(null, obj))
+////        {
+////            return 1;
+////        }
+
+////        if (ReferenceEquals(this, obj))
+////        {
+////            return 0;
+////        }
+
+////        return obj is ValueObjectComparable<T> other ? CompareTo(other) : throw new ArgumentException($"Object must be of type {nameof(ValueObjectComparable<T>)}");
+////    }
+
+////    public int CompareTo(ValueObjectComparable<T>? other)
+////    {
+////        if (ReferenceEquals(null, other))
+////        {
+////            return 1;
+////        }
+
+////        return Value.CompareTo(other.Value);
+////    }
+
+////    public static bool operator <(ValueObjectComparable<T>? left, ValueObjectComparable<T>? right)
+////    {
+////        return Comparer<ValueObjectComparable<T>>.Default.Compare(left, right) < 0;
+////    }
+
+////    public static bool operator >(ValueObjectComparable<T>? left, ValueObjectComparable<T>? right)
+////    {
+////        return Comparer<ValueObjectComparable<T>>.Default.Compare(left, right) > 0;
+////    }
+
+////    public static bool operator <=(ValueObjectComparable<T>? left, ValueObjectComparable<T>? right)
+////    {
+////        return Comparer<ValueObjectComparable<T>>.Default.Compare(left, right) <= 0;
+////    }
+
+////    public static bool operator >=(ValueObjectComparable<T>? left, ValueObjectComparable<T>? right)
+////    {
+////        return Comparer<ValueObjectComparable<T>>.Default.Compare(left, right) >= 0;
+////    }
+////}
