26b975cfa15ad2bd61a381a46eab8ce3cd56b024
Max Obrist <obrist.max@gmail.com>
2022-09-26 21:14:34 +0800

    Some improvements to code generation. auto generate casts

--- SeedWork/src/Domain.Generator/Extensions/SymbolExtensions.cs
+++ SeedWork/src/Domain.Generator/Extensions/SymbolExtensions.cs
@@ -5,11 +5,13 @@ namespace Cranks.SeedWork.Domain.Generator.Extensions;
 internal static class SymbolExtensions
 {
     public static bool IsValueObject(this ISymbol type)
-        => type.GetAttributes()
-               .Any(a => a.AttributeClass is
-                         {
-                             Name: "ValueObjectAttribute",
-                             ContainingAssembly.Name: "Cranks.SeedWork.Domain",
-                             ContainingNamespace.Name: "Attributes",
-                         });
+    {
+        return type.GetAttributes()
+                   .Any(a => a.AttributeClass is
+                             {
+                                 Name: Classes.ValueObjectAttribute,
+                                 ContainingAssembly.Name: Assemblies.Domain,
+                                 ContainingNamespace.Name: "Attributes",
+                             });
+    }
 }
