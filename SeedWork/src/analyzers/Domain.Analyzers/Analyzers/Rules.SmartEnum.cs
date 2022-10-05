6cc8abc9b1495844679ae631ba6e51d4e0d68f4d
Max Obrist <obrist.max@gmail.com>
2022-10-05 20:45:49 +0800

    smart enum base classes

--- SeedWork/src/analyzers/Domain.Analyzers/Analyzers/Rules.SmartEnum.cs
+++ SeedWork/src/analyzers/Domain.Analyzers/Analyzers/Rules.SmartEnum.cs
@@ -11,8 +11,32 @@ internal static partial class Rules
     public static readonly DiagnosticDescriptor SmartEnums_MustNotBeInstantiated
         = new("SEED0101",
               "SmartEnums must not be instantiated outside their defining class",
-              "The smart object '{0}' directly instantiated outside it's definining class. This is not supported.",
+              "The smart enum '{0}' directly instantiated outside it's definining class. This is not supported.",
               Categories.Domain,
               DiagnosticSeverity.Error,
+              true);
+
+    public static readonly DiagnosticDescriptor SmartEnums_DuplicateKey
+        = new("SEED0102",
+              "SmartEnum contains duplicate key",
+              "The smart enum '{0}' contains duplicates for the same key.",
+              Categories.Domain,
+              DiagnosticSeverity.Error,
+              true);
+
+    public static readonly DiagnosticDescriptor SmartEnums_DoNotUseWith
+        = new("SEED0103",
+              "Do not use with expression on smart enums",
+              "The smart enum '{0}' contains duplicates for the same key.", // TODO
+              Categories.Domain,
+              DiagnosticSeverity.Error,
+              true);
+
+    public static readonly DiagnosticDescriptor SmartEnums_ShouldBeSealed
+        = new("SEED0103",
+              "Smart enums should be sealed",
+              "The smart enum '{0}' contains duplicates for the same key.", // TODO
+              Categories.Domain,
+              DiagnosticSeverity.Warning,
               true);
 }
